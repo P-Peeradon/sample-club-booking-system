a9943ab8daa93fa201f05298a45c18508f25d188
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-06-22 16:13:35 +1000

    Setup user

---
+++ scripts/setup_user.sql
@@ -0,0 +1,2 @@
+CREATE USER IF NOT EXISTS 'elmore_student_union'@'localhost' IDENTIFIED BY 'gK9!vX2$mQ7*pL1_zW8$bN4@tY6&fV3%hJ9~rK2&mX5_pZ7$q';
+GRANT ALL PRIVILEGES ON elmore_stop_two.* TO 'elmore_student_union'@'localhost';
